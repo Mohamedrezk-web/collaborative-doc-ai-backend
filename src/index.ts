e30cf38b9b16e390c6ca790a2f3774592391357d
m-rezk-meem <m.Rezk@meemdev.com>
2024-11-22 16:48:20 +0200

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,67 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import OpenAI from 'openai';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+type Bindings = {
+	OPEN_AI_KEY: string;
+	AI: Ai;
+};
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use(
+	'/*',
+	cors({
+		origin: '*',
+		allowHeaders: ['Content-Type', 'Upgrade-Insecure-Requests', 'X-Custom-Header'],
+		allowMethods: ['GET', 'POST', 'PUT', 'OPTIONS'],
+		exposeHeaders: ['X-Kuma-Revision', 'Content-Length'],
+		maxAge: 600,
+		credentials: true,
+	})
+);
+
+app.post('/chat-to-document', async (c) => {
+	const openai = new OpenAI({
+		apiKey: c.env.OPEN_AI_KEY,
+	});
+
+	const { documentData, question } = await c.req.json();
+
+	const chatCompletion = await openai.chat.completions.create({
+		messages: [
+			{
+				role: 'system',
+				content: `You are a helpful assistant that answers questions in the most clear and concise way about ${documentData}.`,
+			},
+			{
+				role: 'user',
+				content: `My question is: ${question}`,
+			},
+		],
+		model: 'gpt-3.5-turbo',
+		temperature: 0.5,
+	});
+
+	const response = chatCompletion.choices[0].message.content;
+
+	return new Response(JSON.stringify({ message: response }));
+});
+
+app.post('/translate-document', async (c) => {
+	const { documentData, targetLanguage } = await c.req.json();
+
+	const summaryResponse = await c.env.AI.run('@cf/facebook/bart-large-cnn', {
+		max_length: 1000,
+		input_text: documentData,
+	});
+
+	const response = await c.env.AI.run('@cf/meta/m2m100-1.2b', {
+		text: summaryResponse.summary,
+		target_lang: targetLanguage,
+		source_lang: 'en',
+	});
+
+	return new Response(JSON.stringify(response));
+});
+
+export default app;
